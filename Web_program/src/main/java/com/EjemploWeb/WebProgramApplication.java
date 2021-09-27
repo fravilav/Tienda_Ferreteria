0be751662c93adfbdec3ea945d8aee0a45d38331
DIANA BOTELLO <DIANA BOTELLO@LAPTOP-IA81Q9BP>
2021-09-27 18:50:40 -0500

    Primer Codigo Compartido 27_09_2021

---
+++ Web_program/src/main/java/com/EjemploWeb/WebProgramApplication.java
@@ -0,0 +1,13 @@
+package com.EjemploWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProgramApplication.class, args);
+	}
+
+}
---
+++ Web_program/src/test/java/com/EjemploWeb/WebProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjemploWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
